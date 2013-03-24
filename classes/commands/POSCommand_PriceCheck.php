644626da3bbbab24a72113c77329be717c8f4d46
Rob Bayliss <rbayliss@rbayliss.net>
2013-03-24 12:34:29 -0400

    Remove dependency on commerce_product_pricing

--- classes/commands/POSCommand_PriceCheck.php
+++ classes/commands/POSCommand_PriceCheck.php
@@ -14,7 +14,15 @@ class POSCommand_PriceCheck extends POS_Command {
 
   public function execute($input, POS_State $state) {
     $product = commerce_product_load_by_sku($input);
-    $price = commerce_product_calculate_sell_price($product);
+
+    if(module_exists('commerce_product_pricing')) {
+      $price = commerce_product_calculate_sell_price($product);
+    }
+    else {
+      $line_item = commerce_product_line_item_new($product, 1);
+      $price = $line_item->commerce_unit_price[LANGUAGE_NONE][0];
+    }
+
     drupal_set_message(t('Price for @name: @price', array(
       '@price' => commerce_currency_format($price['amount'], $price['currency_code'], $product),
       '@name' => entity_label('commerce_product', $product),
