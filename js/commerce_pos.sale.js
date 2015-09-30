41c1a695a80ae545e545c6728c7667ed9a38b15f
srobertson <srobertson@acromediainc.com>
2015-09-30 08:39:16 -0700

    Initial commit of module files.

---
+++ js/commerce_pos.sale.js
@@ -0,0 +1,52 @@
+(function ($) {
+  Drupal.behaviors.commercePosSale = {
+    attach: function (context, settings) {
+      $('.commerce-pos-product-autocomplete', context).each(function () {
+
+        var element = $(this);
+
+        element.once('commerce-pos-autocomplete', function () {
+
+          element.autocomplete({
+            source: settings.commercePosSale.productAutoCompleteUrl
+          });
+
+          // Override the default UI autocomplete render function.
+          element.data('ui-autocomplete')._renderItem = function (ul, item) {
+
+            var innerHtml = $('<div><span>' + item.title + '</span></div>');
+
+            var addButton = $('<a href="#" data-product-sku="' + item.sku + '">Add</a>');
+            addButton.click(function (e) {
+
+              addProductSku($(this).attr('data-product-sku'));
+              e.preventDefault();
+            });
+
+            innerHtml.find('> span').append(addButton);
+
+            return $("<li></li>")
+              .data("item.autocomplete", item)
+              .append(innerHtml)
+              .appendTo(ul);
+          }
+        });
+      });
+
+      $('body', context).each(function () {
+
+        $(this).once('commerce-pos-keybindings')
+      });
+    }
+  };
+
+  /**
+   * Populates the product SKU field on the form and triggers its AJAX event.
+   */
+  var addProductSku = function (sku) {
+    $('.commerce-pos-product-sku-input')
+      .val(sku)
+      .trigger('blur');
+  }
+
+} (jQuery));
