1647649c952de0adbed79ba096d335ac7eba7726
amberbam <abahm@acromediainc.com>
2016-01-19 08:42:33 -0800

    Styling module per Jasons suggestions

--- theme/commerce-pos-keypad.tpl.php
+++ theme/commerce-pos-keypad.tpl.php
@@ -21,15 +21,15 @@
     </div>
     <div class="commerce-pos-keypad-keys">
       <div class="commerce-pos-keypad-numbers">
-        <div class="commerce-pos-keypad-key" data-keybind="1">1</div>
-        <div class="commerce-pos-keypad-key" data-keybind="2">2</div>
-        <div class="commerce-pos-keypad-key" data-keybind="3">3</div>
-        <div class="commerce-pos-keypad-key" data-keybind="4">4</div>
-        <div class="commerce-pos-keypad-key" data-keybind="5">5</div>
-        <div class="commerce-pos-keypad-key" data-keybind="6">6</div>
         <div class="commerce-pos-keypad-key" data-keybind="7">7</div>
         <div class="commerce-pos-keypad-key" data-keybind="8">8</div>
         <div class="commerce-pos-keypad-key" data-keybind="9">9</div>
+        <div class="commerce-pos-keypad-key" data-keybind="4">4</div>
+        <div class="commerce-pos-keypad-key" data-keybind="5">5</div>
+        <div class="commerce-pos-keypad-key" data-keybind="6">6</div>
+        <div class="commerce-pos-keypad-key" data-keybind="1">1</div>
+        <div class="commerce-pos-keypad-key" data-keybind="2">2</div>
+        <div class="commerce-pos-keypad-key" data-keybind="3">3</div>
         <div class="commerce-pos-keypad-key" data-keybind="0">0</div>
         <div class="commerce-pos-keypad-key" data-keybind=".">.</div>
       </div>
