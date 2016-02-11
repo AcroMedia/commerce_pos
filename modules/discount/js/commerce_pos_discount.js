8f59d0ece84edcc419efcb5f42fe9fd6fb4fc05a
Liam Anderson <landerson@acromediainc.com>
2016-02-11 09:47:25 -0800

    Added remove order discount button/functionality

--- modules/discount/js/commerce_pos_discount.js
+++ modules/discount/js/commerce_pos_discount.js
@@ -5,6 +5,12 @@
         console.log(settings);
         $('#commerce-pos-discount-wrapper-' + settings.commercePosDiscount.lineItemId + ' .form-wrapper .form-text').focus();
       }
+
+      $('.commerce-pos-remove-order-discount', context).click(function(event){
+        event.preventDefault();
+
+        $('.commerce-pos-remove-order-discount').trigger('remove_order_discount');
+      });
     }
   };
 
