e6a1ee13541a2cc6bf9d92e7546140f1991c07e0
smccabe <smccabe@289245.no-reply.drupal.org>
2016-05-27 15:38:36 -0700

    Issue #2735399 by smccabe: POS doesn't exist in admin menu

--- theme/commerce-pos-header.tpl.php
+++ theme/commerce-pos-header.tpl.php
@@ -15,7 +15,7 @@
         <?php print t('<em>@username</em> signed in', array('@username' => $account->name)); ?>
       </li>
       <li class="logout">
-        <?php print l(t('sign out'), 'user/logout', array('query' => array('destination' => url('pos')))); ?>
+        <?php print l(t('sign out'), 'user/logout', array('query' => array('destination' => url('admin/commerce/pos')))); ?>
       </li>
     </ul>
   </div>
@@ -23,10 +23,10 @@
   <div class="commerce-pos-header-nav-cont clearfix">
     <div class="commerce-pos-header-links">
       <ul class="clearfix">
-        <li><?php print l(t('Sale'), 'pos/sale'); ?></li>
-        <li><?php print l(t('Return'), 'pos/return'); ?></li>
+        <li><?php print l(t('Sale'), 'admin/commerce/pos/sale'); ?></li>
+        <li><?php print l(t('Return'), 'admin/commerce/pos/return'); ?></li>
         <?php if (module_exists('commerce_pos_report')): ?>
-          <li><?php print l(t('Reports'), 'pos/end-of-day'); ?></li>
+          <li><?php print l(t('Reports'), 'admin/commerce/pos/end-of-day'); ?></li>
         <?php endif; ?>
       </ul>
     </div>
