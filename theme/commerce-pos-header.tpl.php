3b2285b1ac044d9a392704e80f5eb66f6d5a539d
amberbam <amberbam@3358416.no-reply.drupal.org>
2016-05-31 13:04:28 -0700

    Issue #2736841 by amberbam: Remove duplicate logout/admin stuff

--- theme/commerce-pos-header.tpl.php
+++ theme/commerce-pos-header.tpl.php
@@ -9,17 +9,6 @@
 ?>
 
 <div id="commerce-pos-header" class="clearfix">
-  <div class="commerce-pos-header-employee-info">
-    <ul class="clearfix">
-      <li class="username">
-        <?php print t('<em>@username</em> signed in', array('@username' => $account->name)); ?>
-      </li>
-      <li class="logout">
-        <?php print l(t('sign out'), 'user/logout', array('query' => array('destination' => url('admin/commerce/pos')))); ?>
-      </li>
-    </ul>
-  </div>
-
   <div class="commerce-pos-header-nav-cont clearfix">
     <div class="commerce-pos-header-links">
       <ul class="clearfix">
