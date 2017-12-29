60f2680d8f567b51ff35b52e6a760aca608cd685
thejacer87 <thejacer87@3225829.no-reply.drupal.org>
2017-12-29 11:08:12 -0800

    Issue #2933540 by thejacer87: Cashier login form broken

--- src/Form/CashierForm.php
+++ src/Form/CashierForm.php
@@ -14,49 +14,11 @@ class CashierForm extends UserLoginForm {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildForm($form, $form_state);
-
-    // TODO Replace with proper cashier ID field from user type.
-    $form['cashier_id'] = [
-      '#type' => 'textfield',
-      '#title' => t('Cashier ID'),
-      '#description' => t('ID of the cashier logging in'),
-      '#commerce_pos_keypad' => [
-        'type' => 'keypad',
-        'events' => [
-          '.commerce-pos-cashier-login-form-log-in' => [
-            'click' => [],
-          ],
-        ],
-      ],
-    ];
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-    $username = $form_state->getValue('name');
-    $cashier_id = $form_state->getValue('cashier_id');
 
-    if (!empty($username) && !empty($cashier_id)) {
-      $this->storeUser($username, $cashier_id);
-    }
-    else {
-      drupal_set_message(t('Failed to quick store user'), 'error');
-    }
-
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  private function storeUser($username, $id) {
-    Cashiers::storeUser($username, $id);
+    $form_state->setRedirect('commerce_pos.main');
+    return;
   }
 
 }
